5e48abd6bf5de5076fe5c9f7109f5b4360384d3c
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-07-29 16:45:44 +0530

    refactor: Clean up formatting and improve readability in clerkWebHooks function

--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -1,68 +1,53 @@
-import { Webhook} from "svix";
+import { Webhook } from "svix";
 import User from "../models/User.js";
 
-
-// Api Conteroller function to manage cleark with Database
-
-
 export const clerkWebHooks = async (req, res) => {
-try {
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+  try {
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    await whook.verify(JSON.stringify(req.body),{
-        "svix-id": req.headers["svix-id"],
-        "svix-timestamp": req.headers["svix-timestamp"],
-        "svix-signature": req.headers["svix-signature"],
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
     });
 
-    const {data , type} = req.body;
+    const { data, type } = req.body;
 
-    switch (type)
-    {
-        case 'user.created ' : {
-            const userData ={
-                _id : data.id,
-                email : data.email_addresses[0].email_address,
-                name : data.first_name + ' ' + data.last_name,
-                imageUrl: data.image_url
-            }
-            await User.create(userData);
-            res.json({})
-            break;
-        }
-        case 'user.updated ' : {
-            const userData ={
-              
-                email : data.email_addresses[0].email_address,
-                name : data.first_name + ' ' + data.last_name,
-                imageUrl: data.image_url
-            }
-            await User.findbyIdAndUpdate(data.id , userData)
-            res.json({})
-            break;
-        }
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        };
+        await User.create(userData);
+        return res.status(200).json({ success: true });
+      }
 
-        case 'user.deleted ' : {
-            await User.findByIdAndDelete(data.id)
-            res.json({})
-            break;
-        }
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: `${data.first_name} ${data.last_name}`,
+          imageUrl: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        return res.status(200).json({ success: true });
+      }
 
-        default : {
-            res.json({})
-            break;
-        }
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        return res.status(200).json({ success: true });
+      }
 
-
-        
+      default: {
+        return res.status(200).json({ message: "Event ignored" });
+      }
     }
-
-} catch (error) {
-    res.json ({
-        success: false , 
-        message : error.message
-    })
-}
-
-} 
-
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
