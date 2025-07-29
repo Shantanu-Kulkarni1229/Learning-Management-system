ded626ce39d8d6ee9bd1f711e9edf8581b167960
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-07-29 16:32:31 +0530

    refactor: Update server initialization and connection handling

--- server/server.js
+++ server/server.js
@@ -1,26 +1,36 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
-import { clerkWebHooks } from './controllers/webHooks.js';
-import serverless from 'serverless-http';
+import {clerkWebHooks} from './controllers/webHooks.js'
+
+//initialize Express
 
 const app = express();
 
 // Connect to DB
-connectDB()
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
 
-// Middleware
+await connectDB();
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('API working');
-});
-app.post('/clerk', express.json(), clerkWebHooks);
 
-// Export as serverless function
-export const handler = serverless(app);
+//Routes
+app.get('/', (req, res) => {
+    res.send('API working')
+})
+app.post('/clerk' , express.json(), clerkWebHooks) 
+
+
+
+
+//PORT
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
