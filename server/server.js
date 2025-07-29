8769a58a1390e3b5dd9eb7293f0f203edd56f957
Shantanu-Kulkarni1229 <shantanukulkarni1229@gmail.com>
2025-07-29 16:29:25 +0530

    feat: Add serverless-http for serverless deployment and update Vercel configuration

--- server/server.js
+++ server/server.js
@@ -1,36 +1,26 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
 import connectDB from './configs/mongodb.js';
-import {clerkWebHooks} from './controllers/webHooks.js'
-
-//initialize Express
+import { clerkWebHooks } from './controllers/webHooks.js';
+import serverless from 'serverless-http';
 
 const app = express();
 
 // Connect to DB
+connectDB()
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
 
-await connectDB();
-
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-//Routes
+// Routes
 app.get('/', (req, res) => {
-    res.send('API working')
-})
-app.post('/clerk' , express.json(), clerkWebHooks) 
+  res.send('API working');
+});
+app.post('/clerk', express.json(), clerkWebHooks);
 
-
-
-
-//PORT
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
+// Export as serverless function
+export const handler = serverless(app);
